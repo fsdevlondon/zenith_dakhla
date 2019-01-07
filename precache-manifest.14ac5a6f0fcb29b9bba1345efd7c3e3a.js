7cd24366804a87b41d907a7a233ea94793efdf64
Marc Rivelles <rivelles.marc@gmail.com>
2019-01-07 17:45:25 +0000

    Updates

---
+++ precache-manifest.14ac5a6f0fcb29b9bba1345efd7c3e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bc506e73413792ac3b65497b709d52b",
+    "url": "/zenith_dakhla/static/media/zenith_small.5bc506e7.png"
+  },
+  {
+    "revision": "f4d06fb156c8eec1c482c39797851add",
+    "url": "/zenith_dakhla/static/media/zenith_big_logo.f4d06fb1.png"
+  },
+  {
+    "revision": "2062eb38a1035295d162",
+    "url": "/zenith_dakhla/static/js/runtime~main.2062eb38.js"
+  },
+  {
+    "revision": "2f2d08954e3badee8bce",
+    "url": "/zenith_dakhla/static/js/main.2f2d0895.chunk.js"
+  },
+  {
+    "revision": "b28ca23b53b3b0766d03",
+    "url": "/zenith_dakhla/static/js/1.b28ca23b.chunk.js"
+  },
+  {
+    "revision": "67a5e00a6f087fe4b5866afa7f424326",
+    "url": "/zenith_dakhla/index.html"
+  }
+];
